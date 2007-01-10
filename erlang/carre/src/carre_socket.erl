f616835365c8f127772b7cb73d9839245d47992c
cyberlync <cyberlync@b5f09f1a-ec24-0410-abb1-5dd016e7da09>
2007-01-10 23:53:07 +0000

    License changes and attribution
    git-svn-id: https://tercio.googlecode.com/svn/trunk@27 b5f09f1a-ec24-0410-abb1-5dd016e7da09

--- erlang/carre/src/carre_socket.erl
+++ erlang/carre/src/carre_socket.erl
@@ -1,4 +1,19 @@
 %%%-------------------------------------------------------------------
+%%% Copyright 2006 Eric Merritt
+%%%
+%%% Licensed under the Apache License, Version 2.0 (the "License");  
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%       http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%%  Unless required by applicable law or agreed to in writing, software
+%%%  distributed under the License is distributed on an "AS IS" BASIS,
+%%%  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+%%%  implied. See the License for the specific language governing 
+%%%  permissions and limitations under the License.
+%%%
+%%%---------------------------------------------------------------------------
 %%% @author Sean Hinde <seanhinde@users.sourceforge.net>
 %%% @author Eric Merritt <cyberlync@gmail.com>
 %%%
@@ -14,7 +29,7 @@
 %%%  of the total size of incoming http packets but overall its a 
 %%%  worthwile trade off in a small demonstration server like this.
 %%% @end 
-%%%-------------------------------------------------------------------
+%%%---------------------------------------------------------------------------
 -module(carre_socket).
 
 -export([start_link/5]).
